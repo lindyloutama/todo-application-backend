882af3c81fac9bca500d46d8786d3f7cb0c86656
lindyloutama <ltamakaha@yahoo.com>
2019-11-28 16:10:03 +0000

    delete, post added

--- tasks.js
+++ tasks.js
@@ -1,11 +1,32 @@
-'use strict';
+const express = require("express");
+const serverless = require("serverless-http");
+const cors = require("cors");
+const bodyParser = require("body-parser");
 
-module.exports.handler = async (event, context) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: "Go Serverless v1.0! Your function executed successfully!",
-      input: event
-    })
-  };
-};
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.get("/tasks", function(req, res) {
+  res.send({ tasks: ["water plants", "do dishes", "buy oats"] });
+});
+
+app.post("/tasks", function(req, res) {
+  const text = req.body.text;
+  const date = req.body.date;
+
+  res.json({
+    message: `Received a request to add task ${text} with date ${date}`
+  });
+
+app.delete("/tasks/:taskId", function(req, res) {
+    const id = req.params.taskId;
+
+    res.json({
+    message: "request granted"
+    });
+ });
+});
+
+module.exports.handler = serverless(app);
