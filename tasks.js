d926247cd61c8220530372a3ba160d9ff2994b2d
lindyloutama <ltamakaha@yahoo.com>
2019-12-02 22:06:31 +0000

    adding get & put

--- tasks.js
+++ tasks.js
@@ -1,20 +1,42 @@
 const express = require("express");
-const serverless = require("serverless-http");
 const cors = require("cors");
+const serverless = require("serverless-http");
 const bodyParser = require("body-parser");
+const mysql = require("mysql");
 
 const app = express();
 
 app.use(cors());
+// allows Express to parse JSON ddata that is sent on the body of any requests
 app.use(bodyParser.json());
 
-app.get("/tasks", function(req, res) {
-  res.send({ tasks: ["water plants", "do dishes", "buy oats"] });
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: "todos"
+});
+
+app.get("/tasks", function (request, response){
+  connection.query("SELECT * FROM Task", function(err, data) {
+    if (err) {
+      console.log("Error fetching tasks", err);
+      response.status(500).json({
+        error: err
+      });
+    } else {
+      response.status(200).json({
+        tasks: data
+      });
+    }
+  });
 });
 
 app.post("/tasks", function(req, res) {
   const text = req.body.text;
   const date = req.body.date;
+  const completed = req.body.completed;
+  const userId = req.body.completeduserId;
 
   res.json({
     message: `Received a request to add task ${text} with date ${date}`
@@ -29,4 +51,4 @@ app.delete("/tasks/:taskId", function(req, res) {
  });
 });
 
-module.exports.handler = serverless(app);
+module.exports.tasks = serverless(app);
